50f400a641d9717b6b70fbb816495276b967a7e0
Yask Srivastava <yask.srivastava@flipkart.com>
2016-07-27 16:15:45 +0530

    updated

--- content.js
+++ content.js
@@ -3,7 +3,7 @@
  */
 function start() {
     var url  = window.location.href;
-    url = url.replace('https://www.flipkart.com','http://localhost:3000') ;
+    url = url.replace('https://www.flipkart.com','https://cdn.ampproject.org/c/peaceful-peak-42190.herokuapp.com') ;
     window.location = url;
     console.log('working..', url);
 
